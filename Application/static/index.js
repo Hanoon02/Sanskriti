5a9b3447cf5aaaaa4e7690e55f2bf10159f8b17f
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2024-04-13 15:17:29 +0530

    url

--- Application/static/index.js
+++ Application/static/index.js
@@ -1,13 +1,68 @@
+let ImgUploadType = 'Image'; // Default value
+
+function changeUploadType(type) {
+    const imageUploadField = document.getElementById('imageUploadField');
+    if (type === 'Image') {
+        imageUploadField.innerHTML = '<input type="file" id="image_input" name="image_input" accept="image/*" class="block">';
+    } else if (type === 'Link') {
+        imageUploadField.innerHTML = '<input type="text" id="imageLink" name="imageLink" placeholder="Enter image link" class="block w-full border border-black rounded-md shadow-sm py-2 px-2">';
+    }
+    ImgUploadType = type; 
+}
+
+document.getElementById('uploadImageButton').addEventListener('click', function() {
+    changeUploadType('Image');
+});
+
+document.getElementById('uploadLinkButton').addEventListener('click', function() {
+    changeUploadType('Link');
+});
+
 document.getElementById('queryForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var inputData = document.getElementById('input_data').value;
-    var imageInput = document.getElementById('image_input').files[0];
     var languageSelect = document.getElementById('language');
     var selectedLanguage = languageSelect.options[languageSelect.selectedIndex].value;
     var formData = new FormData();
     formData.append('input_data', inputData);
-    formData.append('image_input', imageInput);
     formData.append('language', selectedLanguage); 
+    if (ImgUploadType === 'Link') {
+        var imageLink = document.getElementById('imageLink').value;
+        fetch('/download-image', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ imageUrl: imageLink })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                console.error('Error downloading image:', data.error);
+            } else {
+                fetch(data.imagePath)
+                    .then(response => response.blob())
+                    .then(blob => {
+                        const file = new File([blob], 'image.jpg', { type: 'image/jpeg' });
+                        formData.append('image_input', file);
+                        sendFormData(formData);
+                    })
+                    .catch(error => console.error('Error loading image:', error));
+            }
+        })
+        .catch(error => console.error('Error downloading image:', error));
+    } else {
+        var imageInput = document.getElementById('image_input');
+        if (imageInput) {
+            var imageFile = imageInput.files[0];
+            formData.append('image_input', imageFile);
+        }
+        sendFormData(formData);
+    }
+    
+});
+
+function sendFormData(formData) {
     fetch('/predict', {
         method: 'POST',
         body: formData
@@ -20,7 +75,11 @@ document.getElementById('queryForm').addEventListener('submit', function(event) 
         var resultsContainer = document.getElementById('a');
         resultsContainer.appendChild(resultDiv);
         document.getElementById('input_data').value = '';
-        document.getElementById('image_input').value = '';
+        var imageInput = document.getElementById('image_input');
+        if (imageInput) {
+            imageInput.value = '';
+        }
+        document.getElementById('imageLink').value = '';
     })
     .catch(error => console.error('Error:', error));
-});
+}
